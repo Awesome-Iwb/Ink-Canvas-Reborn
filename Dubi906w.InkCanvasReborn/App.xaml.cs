718d593acd10b269d7d5b65313995e5dda0214f7
kriastans <kriastans@protonmail.com>
2024-11-30 22:53:31 +0800

    | ⬆️更新 | 使用Microsoft.Extension.Hosting 创建了基本的主机应用程序并创建了 Helloworld 主窗口。

---
+++ Dubi906w.InkCanvasReborn/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Dubi906w.InkCanvasReborn {
+    public partial class App : Application {
+
+        public static IHost AppHost;
+
+        public void Application_Startup(object sender, StartupEventArgs e) {
+
+            // 判断配置文件是否存在
+            if (!File.Exists($"{AppContext.BaseDirectory}icr.json")) {
+                File.WriteAllText($"{AppContext.BaseDirectory}icr.json","{}");
+            }
+
+            // 创建 Host
+            AppHost = Host.CreateDefaultBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureAppConfiguration((context, builder) => {
+
+                    builder.AddCommandLine(Program.Args);
+                    builder.AddJsonFile("icr.json",false,true);
+                    builder.AddInMemoryCollection(new [] {
+                        new KeyValuePair<string, string>("instanceID", Guid.NewGuid().ToString())
+                    });
+
+                })
+                .ConfigureServices((context, collection) => {
+                    collection.AddSingleton<MainWindow>();
+                })
+                .Build();
+
+            // 启动
+            AppHost.Start();
+
+            // 显示 MainWindow
+            AppHost.Services.GetService<MainWindow>().Show();
+
+        }
+    }
+}
---
+++ Dubi906w.InkCanvasReborn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dubi906w.InkCanvasReborn.Helpers;
+
+namespace Dubi906w.InkCanvasReborn {
+    
+    internal class Program {
+
+        public static Mutex Mutex = new Mutex(true, "Dubi906w.InkCanvasReborn.Lock");
+        public static string[] Args;
+        public static App Application;
+
+        [STAThread]
+        static void Main(string[] args) {
+
+            // 设置命令行参数
+            Args = args;
+            
+            //判断是否需要显示控制台窗口
+            if (!args.Contains("--console")) ConsoleHelper.HideConsoleWindow();
+            else ConsoleHelper.ShowConsoleWindow();
+
+            ConsoleHelper.ConsoleWrite_InkCanvasRebornASCIIText();
+            ConsoleHelper.ConsoleWrite_Copyright();
+
+            // 启动 App
+            Application = new App();
+            Application.InitializeComponent();
+            Application.Run();
+        }
+    }
+}
